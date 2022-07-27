d48d84d59d8d6fb7de0d10ac3b96b887991ac75e
bijin-pradhan <bijin.pradhan@tigeranalytics.com>
2022-07-27 18:16:29 +0530

    replaced sales model with products

--- assignment4/app/src/app/models/sales.ts
+++
@@ -1,8 +0,0 @@
-export class Sales {
-    constructor(
-        public id: number,
-        public product_name: string,
-        public sales: number,
-        public profit_pct: number
-    ) { }
-}
